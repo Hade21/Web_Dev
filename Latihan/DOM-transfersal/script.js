8cff18c808f66e9465bf9bd4d50739b90f22e125
Hade21 <abdurrohmanmuhammad123@gmail.com>
2022-01-03 14:46:01 +0700

    stoppropagation to stop event bubble

--- Latihan/DOM-transfersal/script.js
+++ Latihan/DOM-transfersal/script.js
@@ -4,10 +4,30 @@
 //     card.style.display = 'none';
 // })
 
-const del = document.querySelectorAll(".hapus")
-del.forEach(function(event) {
-    event.addEventListener("click", function(el) {
+// const del = document.querySelectorAll(".hapus")
+// del.forEach(function(event) {
+//     event.addEventListener("click", function(el) {
+//         el.target.parentElement.style.display = "none";
+//         el.preventDefault();
+//         el.stopPropagation();
+//         console.log(el);
+//     });
+// });
+
+// //event bubbling example
+// const cards = document.querySelectorAll(".card-wrapper");
+// cards.forEach(function(event) {
+//     event.addEventListener("click", function() {
+//         alert("Oke.!");
+//     })
+// })
+
+//modif efektivitas script
+const container = document.querySelector(".container");
+container.addEventListener("click", function(el) {
+    if(el.target.className == "hapus") {
         el.target.parentElement.style.display = "none";
         el.preventDefault();
-    });
-});
+        el.stopPropagation();
+    }
+})
